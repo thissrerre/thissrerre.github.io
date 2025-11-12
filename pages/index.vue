a4406ce58041a3d478ccb90c79911b37dda318bc
thissrerre <renirestiani989@gmail.com>
2025-11-12 22:38:39 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -4,8 +4,8 @@
   <div class="flex w-full h-screen fixed top-0 justify-center items-center">
     <div class="flex h-max w-max justify-center items-center">
       <div class="w-[50%] h-[50%] flex gap-2 flex-col lg:flex-row">
-        <CardLink to="/foolvpn" img="/img/the-fool.png" alt="The Fool Card" />
-        <CardLink to="/nautica" img="/img/the-star.png" alt="The Star Card" />
+        <CardLink to="/raivpn" img="/img/the-fool.png" alt="The Fool Card" />
+        <CardLink to="/raikazu" img="/img/the-star.png" alt="The Star Card" />
       </div>
     </div>
   </div>
